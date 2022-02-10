180e487a6ea183381fa5623f0b23c8f0bf5877ec
Nikita Gugel <googelfist@gmail.com>
2022-02-10 19:04:26 +0500

    edit movieDAO, add methods in local data source,

--- app/src/main/java/com/github/googelfist/moviesearcher/data/datasourse/LocalDataSource.kt
+++ app/src/main/java/com/github/googelfist/moviesearcher/data/datasourse/LocalDataSource.kt
@@ -1,10 +1,14 @@
 package com.github.googelfist.moviesearcher.data.datasourse
 
 import com.github.googelfist.moviesearcher.data.datasourse.local.model.MovieDetailDAO
-import com.github.googelfist.moviesearcher.data.datasourse.local.model.MoviePreviewDAO
+import com.github.googelfist.moviesearcher.data.datasourse.local.model.MoviePreviewListDAO
 
 interface LocalDataSource {
-    suspend fun loadTop250BestFilms(page: Int): MoviePreviewDAO
+    suspend fun loadTop250BestFilms(page: Int): MoviePreviewListDAO
+
+    suspend fun insertMoviePreviewList(moviesPreviewDAO: MoviePreviewListDAO)
 
     suspend fun loadMovieDetail(id: Int): MovieDetailDAO
+
+    suspend fun insertMovieDetail(movieDetail: MovieDetailDAO)
 }
--- app/src/main/java/com/github/googelfist/moviesearcher/data/datasourse/local/MovieDAO.kt
+++ app/src/main/java/com/github/googelfist/moviesearcher/data/datasourse/local/MovieDAO.kt
@@ -6,19 +6,20 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.github.googelfist.moviesearcher.data.datasourse.local.model.MovieDetailDAO
 import com.github.googelfist.moviesearcher.data.datasourse.local.model.MoviePreviewDAO
+import com.github.googelfist.moviesearcher.data.datasourse.local.model.MoviePreviewListDAO
 
 @Dao
 interface MovieDAO {
 
     @Query("SELECT * FROM movie_preview_list_dao WHERE page = :page")
-    suspend fun loadTop250BestFilms(page: Int): List<MoviePreviewDAO>
+    suspend fun loadTop250BestFilms(page: Int): MoviePreviewListDAO
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertMoviePreviewList(page: Int, moviesPreviewDAO: List<MoviePreviewDAO>)
+    suspend fun insertMoviePreviewList(moviesPreviewDAO: MoviePreviewListDAO)
 
     @Query("SELECT * FROM movie_detail WHERE kinopoiskId = :id")
     suspend fun loadMovieDetail(id: Int): MovieDetailDAO
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertMovieDetail(id: Int)
+    suspend fun insertMovieDetail(movieDetailDAO: MovieDetailDAO)
 }
