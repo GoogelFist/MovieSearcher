ab17ae1c287cc9e474c5a348d4f15bcf8d9cfa61
Nikita Gugel <googelfist@gmail.com>
2022-02-10 19:07:07 +0500

    edit for type converters, increase version, fix bag with model class name

--- app/src/main/java/com/github/googelfist/moviesearcher/data/datasourse/local/MovieDataBase.kt
+++ app/src/main/java/com/github/googelfist/moviesearcher/data/datasourse/local/MovieDataBase.kt
@@ -2,15 +2,20 @@ package com.github.googelfist.moviesearcher.data.datasourse.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.github.googelfist.moviesearcher.data.datasourse.local.model.MovieDetailDAO
+import com.github.googelfist.moviesearcher.data.datasourse.local.model.MoviePreviewDAO
 import com.github.googelfist.moviesearcher.data.datasourse.local.model.MoviePreviewListDAO
-import com.github.googelfist.moviesearcher.domain.model.MoviePreview
 
 @Database(
-    entities = [MovieDetailDAO::class, MoviePreview::class, MoviePreviewListDAO::class],
-    version = 1,
+    entities = [MovieDetailDAO::class, MoviePreviewDAO::class, MoviePreviewListDAO::class],
+    version = DB_VERSION,
     exportSchema = false
 )
+
+@TypeConverters(Converters::class)
 abstract class MovieDataBase : RoomDatabase() {
     abstract fun getMovieDao(): MovieDAO
 }
+
+private const val DB_VERSION = 3
