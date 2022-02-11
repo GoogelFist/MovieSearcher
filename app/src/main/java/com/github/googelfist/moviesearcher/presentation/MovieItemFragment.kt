65e8d12ad0c83108e855f854f0db0f2581419b6f
Nikita Gugel <googelfist@gmail.com>
2022-02-11 19:31:41 +0500

    add toolbars, refactor MovieItemFragment.kt

--- app/src/main/java/com/github/googelfist/moviesearcher/presentation/MovieItemFragment.kt
+++ app/src/main/java/com/github/googelfist/moviesearcher/presentation/MovieItemFragment.kt
@@ -1,12 +1,15 @@
 package com.github.googelfist.moviesearcher.presentation
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.github.googelfist.moviesearcher.R
 import com.github.googelfist.moviesearcher.component
 import com.github.googelfist.moviesearcher.databinding.FragmentItemMovieBinding
 import com.google.android.material.snackbar.Snackbar
@@ -41,6 +44,23 @@ class MovieItemFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupToolbar()
+        observeViewModel()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    private fun setupToolbar() {
+        binding.tbFragmentItemMovie.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24)
+        binding.tbFragmentItemMovie.setNavigationOnClickListener {
+            requireActivity().supportFragmentManager.popBackStack()
+        }
+    }
+
+    private fun observeViewModel() {
         mainViewModel.errorMessage.observe(viewLifecycleOwner) {
             it?.let { Snackbar.make(binding.root, it, Snackbar.LENGTH_SHORT).show() }
         }
@@ -53,12 +73,16 @@ class MovieItemFragment : Fragment() {
             binding.tvItemYear.text = it.year
             binding.tvItemDescription.text = it.description
             binding.includeItemFragmentRating.tvItemRating.text = it.ratingKinopoisk
+            setupWebIntent(it.posterUrl)
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
+    private fun setupWebIntent(url: String) {
+        binding.ibFragmentItemWebIntent.setOnClickListener {
+            val webIntent = Intent(Intent.ACTION_VIEW)
+            webIntent.data = Uri.parse(url)
+            startActivity(webIntent)
+        }
     }
 
     companion object {
