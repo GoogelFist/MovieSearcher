4e488bed283677eba36e098072ddf864f6d8b843
Nikita Gugel <googelfist@gmail.com>
2022-04-10 16:59:58 +0500

    make ErrorState classes as data class

--- app/src/main/java/com/github/googelfist/moviesearcher/presentation/states/MovieItemState.kt
+++ app/src/main/java/com/github/googelfist/moviesearcher/presentation/states/MovieItemState.kt
@@ -2,7 +2,7 @@ package com.github.googelfist.moviesearcher.presentation.states
 
 sealed class MovieItemState {
     object NoItemState : MovieItemState()
-    class ErrorState(val message: String): MovieItemState()
+    data class ErrorState(val message: String): MovieItemState()
 
     object UpdatingState : MovieItemState()
     object SuccessState : MovieItemState()
--- app/src/main/java/com/github/googelfist/moviesearcher/presentation/states/MovieListState.kt
+++ app/src/main/java/com/github/googelfist/moviesearcher/presentation/states/MovieListState.kt
@@ -2,7 +2,7 @@ package com.github.googelfist.moviesearcher.presentation.states
 
 sealed class MovieListState {
     object NoListState : MovieListState()
-    class ErrorState(val message: String): MovieListState()
+    data class ErrorState(val message: String): MovieListState()
 
     object UpdatingState : MovieListState()
     object SuccessState : MovieListState()
